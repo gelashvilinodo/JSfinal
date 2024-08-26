5517056184768000c8b1515bd55c3f79d5fb4e1e
gelashvilinodo <166313791+gelashvilinodo@users.noreply.github.com>
2024-08-27 02:29:09 +0400

    darkmode and slider

--- script.js
+++ script.js
@@ -15,3 +15,64 @@ xMark.addEventListener("click", () => {
   xMark.style.display = "none";
   mobileMenu.style.left = "-100%";
 });
+
+// dark mode toggle
+
+const darkMode = document.getElementById("lightMode");
+const header = document.getElementById("header");
+
+let isDarkMode = localStorage.getItem("darkMode");
+
+if (isDarkMode === "enabled") {
+  document.body.classList.add("darkmode");
+  header.classList.add("darkmode");
+}
+
+darkMode.addEventListener("click", () => {
+  isDarkMode = localStorage.getItem("darkMode");
+  if (isDarkMode === "enabled") {
+    localStorage.setItem("darkMode", "disabled");
+    document.body.classList.remove("darkmode");
+    header.classList.remove("darkmode");
+  } else {
+    localStorage.setItem("darkMode", "enabled");
+    document.body.classList.add("darkmode");
+    header.classList.add("darkmode");
+  }
+});
+
+// slider
+
+const sliderItems = document.querySelectorAll(".slider_item");
+const rightButton = document.getElementById("rightBtn");
+const leftButton = document.getElementById("leftBtn");
+
+let activeIndex = 0;
+
+const removeShow = (index) => {
+  sliderItems[index].classList.remove("show");
+};
+
+const addShow = (index) => {
+  sliderItems[index].classList.add("show");
+};
+
+rightButton.addEventListener("click", () => {
+  removeShow(activeIndex);
+  if (activeIndex == sliderItems.length - 1) {
+    activeIndex = 0;
+  } else {
+    activeIndex += 1;
+  }
+  addShow(activeIndex);
+});
+
+leftButton.addEventListener("click", () => {
+  removeShow(activeIndex);
+  if (activeIndex == 0) {
+    activeIndex = sliderItems.length - 1;
+  } else {
+    activeIndex -= 1;
+  }
+  addShow(activeIndex);
+});
